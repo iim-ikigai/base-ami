c18d61418455330a53ecb5b8d267cd4d5ef1747b
iim-ikigai <iim.nurdiansyah@ikigai-io.com>
2023-09-26 17:11:01 +0700

    modify workflow

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -4,7 +4,7 @@
 apt update -y 2>/dev/null | grep packages | cut -d '.' -f 1
 apt install -y aptitude 2>/dev/null | grep packages | cut -d '.' -f 1
 
-aptitude install -y bash python3 python3-pip
+apt install -y bash python3 python3-pip
 python3 -m pip install --upgrade pip wheel setuptools
 # Disable rust due to https://github.com/pyca/cryptography/issues/5776
 python3 -m pip install ansible==${ANSIBLE_VERSION}
