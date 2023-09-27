dc3e851e789c443ada9b2579cdc52e4606b060cc
iim-ikigai <iim.nurdiansyah@ikigai-io.com>
2023-09-27 12:41:16 +0700

    change playbook

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Hide warnings, we'll use aptitude instead of apt later
-apt update -y 2>/dev/null | grep packages | cut -d '.' -f 1
-apt install -y aptitude 2>/dev/null | grep packages | cut -d '.' -f 1
+sudo apt update -y 2>/dev/null | grep packages | cut -d '.' -f 1
+sudo apt install -y aptitude 2>/dev/null | grep packages | cut -d '.' -f 1
 
-apt install -y bash python3 python3-pip
+sudo apt install -y bash python3 python3-pip
 python3 -m pip install --upgrade pip wheel setuptools
 # Disable rust due to https://github.com/pyca/cryptography/issues/5776
 python3 -m pip install ansible
