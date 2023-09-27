9395d94e53c53b7e8435119ed212b06ebc73b76e
iim-ikigai <iim.nurdiansyah@ikigai-io.com>
2023-09-27 10:20:34 +0700

    change playbook

--- scripts/pre-work.sh
+++ scripts/pre-work.sh
@@ -1,1 +1,15 @@
-echo "this is pre work"
+#!/bin/bash
+declare -A osInfo;
+osInfo[/etc/debian_version]="apt"
+osInfo[/etc/alpine-release]="apk"
+osInfo[/etc/centos-release]="yum"
+osInfo[/etc/fedora-release]="dnf"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+sudo $package_manager install python3
