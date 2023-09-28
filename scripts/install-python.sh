56f02356084d818365829d87f1ddedb1f9248720
iim-ikigai <iim.nurdiansyah@ikigai-io.com>
2023-09-28 17:10:28 +0700

    change playbook

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install -y bash python3 python3-pip python3-apt ufw kmod linux-modules-6.2.0-1012-azure linux-modules-6.2.0-1011-azure grub2-common openssh-server cron
+sudo apt install -y bash python3 python3-pip python3-apt ufw kmod grub2-common openssh-server cron
 
 
